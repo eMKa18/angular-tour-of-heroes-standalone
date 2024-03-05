8a9cb3c067f975d561462af5d1895f38189c97de
Monika Gotfryd <monika.gotfryd@nokia.com>
2024-03-05 12:15:53 +0100

    Removal of unused code in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { InMemoryDataService } from './app/in-memory-data.service';
-
-
-// HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {dataEncapsulation: false});
 
 bootstrapApplication(AppComponent, appConfig)
-  .then(() => {
-    // HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, { dataEncapsulation: false });
-  })
   .catch((err) => console.error(err));
